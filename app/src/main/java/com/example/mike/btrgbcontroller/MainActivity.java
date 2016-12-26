ac83d1b1e6ef4548935971ecf3f22e0b9bde9ac9
Majkel44 <m.werner9205@gmail.com>
2016-12-26 22:45:27 +0100

    - added to MainActivity HSVColorPicker and implemented button responsible for showing ColorPicker - added variable for containing hsv and rgb color - added variable for containing adress of bluetooth device - added button to showing DeviceList - added handling of connecting with bluetooh device

--- app/src/main/java/com/example/mike/btrgbcontroller/MainActivity.java
+++ app/src/main/java/com/example/mike/btrgbcontroller/MainActivity.java
@@ -1,13 +1,155 @@
 package com.example.mike.btrgbcontroller;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+   private float[] hsv = new float[3];
+   private int[] rgb = new int[3];
+    private ProgressDialog progress;
+   private String adress = " ";
+   private BluetoothAdapter my_bt = null;
+   private BluetoothSocket sock_bt = null;
+   private boolean is_connected = false;
+   static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent deviceList = getIntent();
+        adress = deviceList.getStringExtra(DeviceList.EXTRA_ADRESS);
+
+
         setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.button);
+        Button blueBtn = (Button)findViewById(R.id.button4);
+        final TextView text = (TextView)findViewById(R.id.textView3);
+        text.setText("Adress: "+adress);
+
+        if(adress != null){
+
+            new ConnectBT().execute();
+
+        }
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                HSVColorPickerDialog cp = new HSVColorPickerDialog(MainActivity.this, 0xFF4488CC, new HSVColorPickerDialog.OnColorSelectedListener() {
+                    @Override
+                    public void colorSelected(Integer color) {
+
+                        Color.colorToHSV(color,hsv);
+
+                        //text.setText("H: "+Float.toString(hsv[0])+" S: "+Float.toString(hsv[1]*100)+" V: "+Float.toString(hsv[2]*100));
+
+
+
+
+
+                    }
+                });
+
+
+                cp.setTitle("Paleta HSV");
+
+                cp.show();
+
+
+            }
+        });
+
+        blueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent deviceListx = new Intent(getApplicationContext(),DeviceList.class);
+                startActivity(deviceListx);
+
+            }
+        });
+
+    }
+
+    private class ConnectBT extends AsyncTask<Void,Void,Void>{
+
+        private boolean connectSucces = true;
+
+
+        @Override
+        protected void onPreExecute() {
+
+            progress = ProgressDialog.show(MainActivity.this,"Connecting...","Please Wait...");
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) {
+
+            try{
+
+                if(sock_bt==null || !is_connected){
+
+                    my_bt = BluetoothAdapter.getDefaultAdapter();
+                    BluetoothDevice my_device = my_bt.getRemoteDevice(adress);
+                    sock_bt = my_device.createInsecureRfcommSocketToServiceRecord(myUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    sock_bt.connect();
+
+                }
+
+            }catch (IOException e){
+
+                Toast.makeText(getApplicationContext(),"Err "+e.toString(),Toast.LENGTH_LONG).show();
+
+
+
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            if(!connectSucces){
+
+                Toast.makeText(getApplicationContext(),"Connection Failed",Toast.LENGTH_LONG);
+            }else{
+
+                Toast.makeText(getApplicationContext(),"Connected",Toast.LENGTH_LONG);
+                is_connected = true;
+            }
+
+            progress.dismiss();
+
+
+        }
     }
 }
+
+
